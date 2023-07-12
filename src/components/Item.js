35cffb193d928a6c7679e226ec4b74222ab0aec5
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-12 18:40:14 +0700

    implement logic onto component Calculator

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,37 @@
 import PropTypes from 'prop-types';
+import calculate from './logic/calculate';
 
-export default function Item({ sign, index }) {
-  return <div className={`item-${index} items-shared`}>{sign}</div>;
+export default function Item({
+  sign, index, total, next, operation, setExpression,
+}) {
+  const setExpressionAtItem = (e) => {
+    const res = calculate({ total, next, operation }, e.currentTarget.textContent);
+    setExpression(res);
+  };
+
+  if (index) {
+    return (
+      <button
+        type="button"
+        className={`item-${index} items-shared`}
+        onClick={setExpressionAtItem}
+      >
+        {sign}
+      </button>
+    );
+  }
+  return (
+    <div className={`item-${index} items-shared`}>
+      {next || total}
+    </div>
+  );
 }
 
 Item.propTypes = {
   sign: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
+  total: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+  setExpression: PropTypes.func.isRequired,
 };
