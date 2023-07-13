8b3adb81d9a2e8f4020a6981fe32cd62e2a57660
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-14 00:30:18 +0700

    change to ramdom quote API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import FetchAPI from './components/utils/FetchAPI';
 import Calculator from './components/Calculator';
-import NavigationBar from './components/utils/NavigationBar';
 
 import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <FetchAPI />
       <Calculator />
+      <FetchAPI />
     </div>
   );
 }
--- src/components/utils/FetchAPI.js
+++ src/components/utils/FetchAPI.js
@@ -1,25 +1,19 @@
 import { useEffect, useState } from 'react';
 import './utils.css';
 
-const API = 'https://api.api-ninjas.com/v1/logo?name=';
+const API = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
 const API_KEY = '8Dn4moNXRBFtBy/+FxeqaA==gcfYWmrBwNeX4ZuL';
 
 export default function FetchAPI() {
   const [isLoading, setIsLoading] = useState(false);
-  const [org, setOrg] = useState(null);
-  const [logo, setLogo] = useState(null);
+  const [quote, setQuote] = useState(null);
   const [message, setMessage] = useState(null);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    setOrg(form.org.value);
-    form.reset();
-  };
 
   useEffect(() => {
     const fetchAPI = async () => {
+      setIsLoading(true);
       try {
-        const res = await fetch(API + org, {
+        const res = await fetch(API, {
           method: 'GET',
           headers: {
             'X-API-KEY': API_KEY,
@@ -28,42 +22,30 @@ export default function FetchAPI() {
 
         const [result] = await res.json();
         if (result) {
-          setLogo(result.image);
+          setQuote(result.quote);
           setMessage(null);
-          setIsLoading(false);
         } else {
-          setMessage(`There is no logo for this org "${org}", please add another one`);
-          setTimeout(() => setMessage(null), 4000);
-          setIsLoading(false);
+          setMessage('There is some error');
+          setQuote(null);
         }
+
+        setIsLoading(false);
       } catch (err) {
         setIsLoading(false);
-        setMessage(null);
         setMessage(err);
       }
     };
 
-    if (org !== null) {
-      setIsLoading(true);
-      setLogo(null);
-      fetchAPI();
-    }
-  }, [org]);
+    fetchAPI();
+  }, []);
 
-  if (isLoading) return (<div>Loading...</div>);
+  if (isLoading) return (<div className="fetch-api">Loading...</div>);
 
   return (
     <div className="fetch-api">
-      <h5 className="api-header">When you input an organization name, it will show the logo of the organization</h5>
-
-      <form onSubmit={handleSubmit}>
-        <input name="org" className="input" placeholder="organization name" type="text" required />
-
-        <input type="submit" className="items-shared" id="submit-btn" value="submit" />
-      </form>
 
       {
-        logo && <img src={logo} className="logo" alt="logo" width="200" />
+        quote && <p className="message">{quote}</p>
       }
 
       {
