4043b29b09011bf254e916aab89e2d379db3708b
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-13 23:33:29 +0700

    build fetchAPI - client needs to input a name of an organization. - it will show a logo of an organization if the API can find it - if there is no logo, it will show a message saying "There is no logo"

--- src/components/utils/FetchAPI.js
+++ src/components/utils/FetchAPI.js
@@ -1,7 +1,75 @@
+import { useEffect, useState } from 'react';
 import './utils.css';
 
+const API = 'https://api.api-ninjas.com/v1/logo?name=';
+const API_KEY = '8Dn4moNXRBFtBy/+FxeqaA==gcfYWmrBwNeX4ZuL';
+
 export default function FetchAPI() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [org, setOrg] = useState(null);
+  const [logo, setLogo] = useState(null);
+  const [message, setMessage] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    setOrg(form.org.value);
+    form.reset();
+  };
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      try {
+        const res = await fetch(API + org, {
+          method: 'GET',
+          headers: {
+            'X-API-KEY': API_KEY,
+          },
+        });
+
+        const [result] = await res.json();
+        if (result) {
+          setLogo(result.image);
+          setMessage(null);
+          setIsLoading(false);
+        } else {
+          setMessage(`There is no logo for this org "${org}", please add another one`);
+          setTimeout(() => setMessage(null), 4000);
+          setIsLoading(false);
+        }
+      } catch (err) {
+        setIsLoading(false);
+        setMessage(null);
+        setMessage(err);
+      }
+    };
+
+    if (org !== null) {
+      setIsLoading(true);
+      setLogo(null);
+      fetchAPI();
+    }
+  }, [org]);
+
+  if (isLoading) return (<div>Loading...</div>);
+
   return (
-    <div className="fetch-api">API</div>
+    <div className="fetch-api">
+      <h5 className="api-header">When you input an organization name, it will show the logo of the organization</h5>
+
+      <form onSubmit={handleSubmit}>
+        <input name="org" className="input" placeholder="organization name" type="text" required />
+
+        <input type="submit" className="items-shared" id="submit-btn" value="submit" />
+      </form>
+
+      {
+        logo && <img src={logo} className="logo" alt="logo" width="200" />
+      }
+
+      {
+        message && <div className="message">{message}</div>
+      }
+
+    </div>
   );
 }
--- src/components/utils/NavigationBar.js
+++ src/components/utils/NavigationBar.js
@@ -8,14 +8,16 @@ export default function NavigationBar() {
     components.forEach((item) => {
       if (item === e.currentTarget.name) {
         document.querySelector(`.${item}`).style.visibility = 'visible';
+        document.querySelector(`.${item}`).style.position = 'static';
       } else {
         document.querySelector(`.${item}`).style.visibility = 'hidden';
+        document.querySelector(`.${item}`).style.position = 'fixed';
       }
     });
   };
 
   return (
-    <div className="navigation-bar">
+    <div id="navigation-bar">
       {
       components.map((e) => (
         <button className="items-shared nav-button" key={uuidv4()} type="button" name={e} onClick={navigate}>{e}</button>
