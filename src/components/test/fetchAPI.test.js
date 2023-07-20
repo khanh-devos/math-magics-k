517ea9d14e903eeea28e5add9113aafa2726d3e2
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-20 16:09:04 +0700

    fix linter errors

--- src/components/test/fetchAPI.test.js
+++ src/components/test/fetchAPI.test.js
@@ -1,42 +1,41 @@
-import FetchAPI from '../utils/FetchAPI';
 import { render, act } from '@testing-library/react';
-
+import FetchAPI from '../utils/FetchAPI';
 
 describe('fetchAPI test', () => {
-    // mock if dom container has been rendered
-    it('should match the snapshot', () => {
-        const { container } = render(<FetchAPI />);
-        expect(container).toMatchSnapshot();
-    });
+  // mock if dom container has been rendered
+  it('should match the snapshot', () => {
+    const { container } = render(<FetchAPI />);
+    expect(container).toMatchSnapshot();
+  });
 
-    it('should render json()', async () => {
-        jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-          json: () => Promise.resolve([{ quote: 'Khanh' }]),
-        }));
-    
-        await act(async () => {
-          const { asFragment } = render(<FetchAPI />);
-          expect(asFragment).toMatchSnapshot();
-        });
-    });
+  it('should render json()', async () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve([{ quote: 'Khanh' }]),
+    }));
 
-    it('should render message', async () => {
-        jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-          json: () => Promise.resolve([]),
-        }));
-    
-        await act(async () => {
-          const { asFragment } = render(<FetchAPI />);
-          expect(asFragment).toMatchSnapshot();
-        });
+    await act(async () => {
+      const { asFragment } = render(<FetchAPI />);
+      expect(asFragment).toMatchSnapshot();
     });
+  });
 
-    it('should render ERROR', async () => {
-        jest.spyOn(global, 'fetch').mockImplementationOnce(() => "");
-    
-        await act(async () => {
-          const { asFragment } = render(<FetchAPI />);
-          expect(asFragment).toMatchSnapshot();
-        });
+  it('should render message', async () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve([]),
+    }));
+
+    await act(async () => {
+      const { asFragment } = render(<FetchAPI />);
+      expect(asFragment).toMatchSnapshot();
     });
-})
+  });
+
+  it('should render ERROR', async () => {
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => '');
+
+    await act(async () => {
+      const { asFragment } = render(<FetchAPI />);
+      expect(asFragment).toMatchSnapshot();
+    });
+  });
+});
